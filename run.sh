e782202e82e33ecf5d487bd03d5f3a1e00616650
Jing kai Ooi <ooijingkai@gmail.com>
2023-03-22 00:59:44 +0800

    Remove kafka connectui

--- run.sh
+++ run.sh
@@ -1,6 +1,10 @@
 curl -i --max-time 60 --retry-connrefused --retry 10 -X PUT -H "Accept:application/json" -H  "Content-Type:application/json" http://connect:8083/connectors/source-csv-spooldir-03/config -d '{
   "connector.class": "com.github.jcustenborder.kafka.connect.spooldir.SpoolDirCsvSourceConnector",
-  "task.max": "2",
+  "tasks.max": "2",
+  "producer.acks":"1",
+  "producer.batch.size":"32768",
+  "batch.size":"1000",
+  "enable.idempotence":"false",
   "cleanup.policy": "DELETE",
   "topic": "ftptransactions",
   "input.path": "/data/unprocessed",
@@ -10,13 +14,13 @@ curl -i --max-time 60 --retry-connrefused --retry 10 -X PUT -H "Accept:applicati
   "key.schema": "{\"name\":\"com.github.jcustenborder.kafka.connect.model.Key\",\"type\":\"STRUCT\",\"isOptional\":false,\"fieldSchemas\":{\"id\":{\"type\":\"STRING\",\"isOptional\":true}}}",
   "value.schema": "{\"name\":\"com.github.jcustenborder.kafka.connect.model.Value\",\"type\":\"STRUCT\",\"isOptional\":false,\"fieldSchemas\":{\"id\":{\"type\":\"STRING\",\"isOptional\":true},\"transaction_id\":{\"type\":\"STRING\",\"isOptional\":true},\"merchant\":{\"type\":\"STRING\",\"isOptional\":true},\"mcc\":{\"type\":\"STRING\",\"isOptional\":true},\"currency\":{\"type\":\"STRING\",\"isOptional\":true},\"amount\":{\"type\":\"STRING\",\"isOptional\":true},\"transaction_date\":{\"type\":\"STRING\",\"isOptional\":true},\"card_id\":{\"type\":\"STRING\",\"isOptional\":true},\"card_pan\":{\"type\":\"STRING\",\"isOptional\":true},\"card_type\":{\"type\":\"STRING\",\"isOptional\":true}}}","value.converter":"org.apache.kafka.connect.json.JsonConverter",
   "value.converter.schemas.enable":"false",
-  "producer.override.enable.idempotence":"true",
-  "producer.override.acks":"all",
+  "producer.override.acks":"1",
+  "producer.override.batch.size":"32768",
   "transforms": "cast,setDefaultValueToInt",
   "transforms.cast.type": "org.apache.kafka.connect.transforms.Cast$Value",
   "transforms.cast.spec": "amount:float64",
   "transforms.setDefaultValueToInt.type": "org.apache.kafka.connect.transforms.ReplaceField$Value",
-"transforms.setDefaultValueToInt.field": "mcc",
-"transforms.setDefaultValueToInt.default": 0,
-"transforms.setDefaultValueToInt.type.to": "int32"
+  "transforms.setDefaultValueToInt.field": "mcc",
+  "transforms.setDefaultValueToInt.default": 0,
+  "transforms.setDefaultValueToInt.type.to": "int32"
 }'
