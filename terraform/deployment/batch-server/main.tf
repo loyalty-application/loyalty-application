11e0220e2616904fb57f50675de329f24916d522
soonann <sudo@soonann.dev>
2023-03-23 23:49:30 +0800

    feat: deployed sftp-txn

--- terraform/deployment/batch-server/main.tf
+++ terraform/deployment/batch-server/main.tf
@@ -79,43 +79,6 @@ resource "aws_ecs_task_definition" "init_kafka" {
   ])
 }
 
-# ecs task definition
-#resource "aws_ecs_task_definition" "this" {
-#family = "${var.project.name}-task-def"
-#volume {
-#name = "efsVolume"
-#efs_volume_configuration {
-#transit_encryption = "DISABLED"
-#file_system_id     = var.efs.file_system_id
-#root_directory     = "/"
-#}
-#}
-#container_definitions = jsonencode([
-#{
-#name              = local.container_name
-#image             = local.container_image
-#memoryReservation = 256
-##portMappings = [
-##{
-##containerPort = local.container_port
-##hostPort      = 0
-##}
-##]
-#environment = [
-#for k, v in var.ENV : { name = k, value = v }
-#]
-#command = [
-#]
-#mountPoints : [
-#{
-#sourceVolume  = "efsVolume",
-#containerPath = "/data",
-#}
-#]
-#}
-#])
-#}
-
 # onetime tasks
 data "aws_ecs_task_execution" "this" {
   cluster         = module.ecs_ec2.cluster.id
@@ -130,5 +93,80 @@ data "aws_ecs_task_execution" "this" {
     weight            = 100
     capacity_provider = module.ecs_ec2.cp.name
   }
+}
+
+# ----------------------------------------------------------------------
+# ecs task definition
+resource "aws_ecs_task_definition" "go_sftp_txn" {
+  family = "go-sftp-txn-task-def"
+  volume {
+    name = "efsVolume"
+    efs_volume_configuration {
+      transit_encryption = "DISABLED"
+      file_system_id     = var.efs.file_system_id
+      root_directory     = "/"
+    }
+  }
+  container_definitions = jsonencode([
+    {
+      name              = "go-sftp-txn"
+      image             = "docker.io/loyaltyapplication/go-sftp-txn:latest"
+      memoryReservation = 256
+      environment = [
+        for k, v in var.ENV : { name = k, value = v }
+      ]
+      mountPoints : [
+        {
+          sourceVolume  = "efsVolume",
+          containerPath = "/data",
+        }
+      ]
+    }
+  ])
+}
+
+# iam role
+resource "aws_iam_role" "ecs_events" {
+  name               = "ecs_events"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+# iam policy
+resource "aws_iam_role_policy" "ecs_events_run_task_with_any_role" {
+  name   = "ecs_events_run_task_with_any_role"
+  role   = aws_iam_role.ecs_events.id
+  policy = data.aws_iam_policy_document.ecs_events_run_task_with_any_role.json
+}
+
+# cloudwatch event rule
+resource "aws_cloudwatch_event_rule" "this" {
+  name                = "go-sftp-txn-cron-rule"
+  description         = "Cron Job to run go-sftp-txn"
+  schedule_expression = "rate(30 minutes)"
+}
+
+
+# cloudwatch event target
+resource "aws_cloudwatch_event_target" "ecs_scheduled_task" {
+  target_id = "go-sftp-txn-job"
+  arn       = module.ecs_ec2.cluster.arn
+  rule      = aws_cloudwatch_event_rule.this.name
+  role_arn  = aws_iam_role.ecs_events.arn
+
+  ecs_target {
+    task_count          = 1
+    task_definition_arn = aws_ecs_task_definition.go_sftp_txn.arn
+  }
+  input = jsonencode({
+    containerOverrides = [
+      {
+        name = "go-sftp-txn",
+        command = [
+          "2021-08-27"
+        ]
+      }
+    ]
+  })
+
 }
 
