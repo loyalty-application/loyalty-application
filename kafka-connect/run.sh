438130d1ed0996ef81b763290c2021261119d989
soonann <sudo@soonann.dev>
2023-04-04 20:00:01 +0800

    chore: modified kafka-connect container

--- kafka-connect/run.sh
+++ kafka-connect/run.sh
@@ -4,7 +4,7 @@ BINARY_PATH=./connect
 
 # Run the binary in a loop until it returns a successful exit code
 # change to another env var
-while [ $(hostname) = "connect" ]; do
+while [ $CONNECT = "SFTP_NODE" ]; do
   $BINARY_PATH "$@"
   if [ $? -eq 0 ]; then
     echo "Binary ran successfully"
