d9ba87040e9ce4068306d6e3c8430274e37f007c
ooijingkai10 <ooijingkai@gmail.com>
2023-04-04 20:13:01 +0000

    fixed issue where same files are downloaded

--- kafka-connect/run.sh
+++ kafka-connect/run.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-BINARY_PATH=./connect
+BINARY_PATH="./connect"
 
-# Run the binary in a loop until it returns a successful exit code
-# change to another env var
-while [ $CONNECT = "SFTP_NODE" ]; do
+# Use double quotes to prevent word splitting and globbing
+# Change to another env var, e.g. CONNECT_TYPE
+while [ "$CONNECT" = "SFTP_NODE" ] && [ ! -e "./spend/$@"* ] && [ ! -e "./users/$@"* ]; do
+  # Use "$@" to pass all command-line arguments as separate arguments to the binary
   $BINARY_PATH "$@"
   if [ $? -eq 0 ]; then
     echo "Binary ran successfully"
@@ -12,4 +13,3 @@ while [ $CONNECT = "SFTP_NODE" ]; do
   fi
   echo "Binary returned non-zero exit code, restarting..."
 done
-
